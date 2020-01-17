25d7b2359d98a47ff03019f06550d1b5a9419103
Manish <manishkumar13899@gmail.com>
2020-01-17 22:49:17 +0530

    Updates

---
+++ precache-manifest.21bd5e4aaa569f31d152818592621830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9acf132f1df2543226c92d13a7a5507",
+    "url": "/MkTube/index.html"
+  },
+  {
+    "revision": "5754d11a7ad4ad039f97",
+    "url": "/MkTube/static/css/main.666d9824.chunk.css"
+  },
+  {
+    "revision": "eaf8928cd378e8125a59",
+    "url": "/MkTube/static/js/2.c2bc3885.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/MkTube/static/js/2.c2bc3885.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5754d11a7ad4ad039f97",
+    "url": "/MkTube/static/js/main.a5373223.chunk.js"
+  },
+  {
+    "revision": "77ffd6e78904ee4e5a85",
+    "url": "/MkTube/static/js/runtime-main.792a3dc0.js"
+  }
+]);
